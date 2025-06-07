9efb994409bcf8ced6b7b54940887c2fb3fbd277
Andrew-glitch-Ux <muntetandrew04@gmail.com>
2025-06-07 19:57:46 +0300

    Fix: updated main.tf configuration for Day 8

--- Day8/Submissions/Andrew-glitch-Ux/lab10workspace/main.tf
+++ Day8/Submissions/Andrew-glitch-Ux/lab10workspace/main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
-  region = var.aws_region
+  region = "eu-north-1"
 }
 
 resource "aws_s3_bucket" "workspace_demo" {
-  bucket = "workspace-demo-${terraform.workspace}"  # Bucket name changes per workspace
-  acl    = "private"
+  bucket = "workspace-demo-${terraform.workspace}"
+}
+
+resource "aws_s3_bucket_public_access_block" "workspace_demo_block" {
+  bucket                  = aws_s3_bucket.workspace_demo.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
