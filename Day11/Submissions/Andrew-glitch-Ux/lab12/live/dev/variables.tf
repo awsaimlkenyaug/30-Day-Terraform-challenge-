311329ab50f0f664301ac0558a7475b4ba212351
Andrew-glitch-Ux <muntetandrew04@gmail.com>
2025-06-10 04:04:00 +0300

    Day 11: Added Lab 13 infra, diagrams, and submission files

---
+++ Day11/Submissions/Andrew-glitch-Ux/lab12/live/dev/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {}
+variable "region" {}
+variable "vpc_cidr" {}
+variable "subnet_cidr" {}
+variable "az" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "route_cidr" {}
+
+variable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "egress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "deploy_instance" {
+  type    = bool
+  default = true
+}
