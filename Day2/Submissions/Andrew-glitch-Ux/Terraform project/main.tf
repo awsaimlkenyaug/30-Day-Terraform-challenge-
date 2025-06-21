82967fe2ccce9a746abcd22c8a95093c6acc5781
Andrew-glitch-Ux <muntetandrew04@gmail.com>
2025-06-21 03:31:15 +0300

    Save current changes before switching to week4 branch

--- Day2/Submissions/Andrew-glitch-Ux/Terraform project/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "eu-north-1"
-}
-resource "aws_instance" "example" {
-  ami           = "ami-0c1ac8a41498c1a9c" 
-  instance_type = "t3.micro"
-  subnet_id     = "subnet-0b313224ecb43decc"
-}
