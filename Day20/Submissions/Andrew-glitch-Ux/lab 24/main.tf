ea894c132d8f8e08b0c141f46c0f6bc413222ef5
Andrew-glitch-Ux <muntetandrew04@gmail.com>
2025-06-26 23:51:35 +0300

    Test: Trigger Sentinel policy violation

---
+++ Day20/Submissions/Andrew-glitch-Ux/lab 24/main.tf
@@ -0,0 +1,63 @@
+
+# Trigger apply from GitHub commit
+terraform {
+  cloud {
+    organization = "tf-challenge" 
+    workspaces {
+      name = "30-Day-Terraform-challenge-lab23"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = var.allowed_ssh_cidr
+}
+
+egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = var.egress_cidr
+}
+}
+
+resource "aws_instance" "lab_instance" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.main.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = var.instance_name
+  }
+}
