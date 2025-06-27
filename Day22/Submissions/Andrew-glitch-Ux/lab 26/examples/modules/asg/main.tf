f0a99b794105caaaa8b97053a7d1126ed3d9d62c
Andrew-glitch-Ux <muntetandrew04@gmail.com>
2025-06-27 20:57:09 +0300

    Day 24: Security Best Practices setup with inline comments and validations

---
+++ Day22/Submissions/Andrew-glitch-Ux/lab 26/examples/modules/asg/main.tf
@@ -0,0 +1,60 @@
+resource "aws_launch_template" "this" {
+  name_prefix   = "${var.name}-lt-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+
+
+  network_interfaces {
+  associate_public_ip_address = false
+  security_groups             = var.asg_security_group_ids
+}
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = var.tags
+  }
+}
+
+resource "aws_autoscaling_group" "this" {
+  name                      = "${var.name}-asg"
+  min_size                  = var.min_size
+  max_size                  = var.max_size
+  desired_capacity          = var.desired_capacity
+  vpc_zone_identifier       = var.subnet_ids
+  health_check_type         = "EC2"
+  health_check_grace_period = var.health_check_grace_period
+  target_group_arns         = var.target_group_arns
+
+  launch_template {
+    id      = aws_launch_template.this.id
+    version = "$Latest"
+  }
+
+  # Static name tag
+  tag {
+    key                 = "Name"
+    value               = "${var.name}-asg-instance"
+    propagate_at_launch = true
+  }
+
+  # Dynamic tags from var.tags
+  dynamic "tag" {
+    for_each = var.tags
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
---
+++ Day22/Submissions/Andrew-glitch-Ux/lab 26/examples/modules/vpc/main.tf
@@ -0,0 +1,51 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-vpc"
+  })
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-igw"
+  })
+}
+
+resource "aws_subnet" "public" {
+  for_each = var.public_subnets
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.az
+  map_public_ip_on_launch = true
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-public-subnet-${each.key}"
+  })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-public-rt"
+  })
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
