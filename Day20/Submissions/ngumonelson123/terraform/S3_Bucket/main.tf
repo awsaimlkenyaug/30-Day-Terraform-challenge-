bce1ff89483fc5a1ae31bc0de62b5c1acd679cb2
ngumonelson123 <nelsonmbui88@gmail.com>
2025-06-29 19:10:31 +0300

    Allow destroy for cleanup

--- Day20/Submissions/ngumonelson123/terraform/S3_Bucket/main.tf
+++ Day20/Submissions/ngumonelson123/terraform/S3_Bucket/main.tf
@@ -6,6 +6,10 @@ provider "aws" {
 resource "aws_s3_bucket" "my_app_bucket" {
   bucket = "ngumo-app-bucket-${random_id.bucket_id.hex}"
   acl    = "private"
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "random_id" "bucket_id" {
