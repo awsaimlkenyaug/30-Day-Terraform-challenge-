a9e735b66c4e13514c735db8150668251f11f301
ngumonelson123 <nelsonmbui88@gmail.com>
2025-06-29 18:49:25 +0300

    Day 20: initial commit - deploy app code (S3)

---
+++ Day20/Submissions/ngumonelson123/terraform/S3_Bucket/main.tf
@@ -0,0 +1,13 @@
+# main.tf
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_app_bucket" {
+  bucket = "ngumo-app-bucket-${random_id.bucket_id.hex}"
+  acl    = "private"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
