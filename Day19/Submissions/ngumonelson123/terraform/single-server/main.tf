1cd6207ed9e5e4349bb1c248cf0523ff1f3e0614
ngumonelson123 <nelsonmbui88@gmail.com>
2025-06-29 16:58:03 +0300

    Add Day 19 - IaC adoption plan and Terraform Cloud deployments

---
+++ Day19/Submissions/ngumonelson123/terraform/single-server/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "remote" {
+    organization = "nelson_hcp"
+
+    workspaces {
+      name = "day19-single-server"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Day19Server"
+  }
+}
