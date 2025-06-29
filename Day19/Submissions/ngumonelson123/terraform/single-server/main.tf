f786d59e5174bfbe09c511f5c769b7628c06a448
ngumonelson123 <nelsonmbui88@gmail.com>
2025-06-29 17:49:23 +0300

    Fix: use aws_subnet instead of aws_subnet_ids

--- Day19/Submissions/ngumonelson123/terraform/single-server/main.tf
+++ Day19/Submissions/ngumonelson123/terraform/single-server/main.tf
@@ -12,20 +12,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Get default VPC
+# Fetch default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Get default subnet in that VPC
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+# Fetch first available subnet in the default VPC
+data "aws_subnet" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  # Optional filter to narrow to public subnet
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-080e1f13689e07408"
   instance_type = "t2.micro"
-  subnet_id     = tolist(data.aws_subnet_ids.default.ids)[0]  # pick the first subnet
+  subnet_id     = data.aws_subnet.default.id
 
   tags = {
     Name = "Day19Server"
