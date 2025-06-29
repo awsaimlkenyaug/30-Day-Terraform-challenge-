0bf7e02266e4fdd7de5b4763731415f4101183db
ngumonelson123 <nelsonmbui88@gmail.com>
2025-06-29 17:48:04 +0300

    Fix: specify subnet for Day19 EC2 deployment

--- Day19/Submissions/ngumonelson123/terraform/single-server/main.tf
+++ Day19/Submissions/ngumonelson123/terraform/single-server/main.tf
@@ -12,12 +12,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get default subnet in that VPC
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-080e1f13689e07408"
   instance_type = "t2.micro"
+  subnet_id     = tolist(data.aws_subnet_ids.default.ids)[0]  # pick the first subnet
 
   tags = {
     Name = "Day19Server"
   }
 }
-# Triggering Day 19 run
